ba4ba6cf4a5983d18484603f557fd11a863c03fa
julien.spenst <julienspenst06@gmail.com>
2023-05-08 15:02:21 +0200

    .

--- src/Kugelfangen.java
+++ src/Kugelfangen.java
@@ -9,7 +9,7 @@ public class Kugelfangen {
 
     public Kugelfangen() {
         kamera = new GLEntwicklerkamera();
-        kamera.setzePosition(0, 500, 800);
+        kamera.setzePosition(0, 1000, 1800);
 
         licht = new GLLicht();
         himmel = new GLHimmel("src/img/Sterne.jpg");
@@ -17,23 +17,23 @@ public class Kugelfangen {
 
         bmw=new Bmw();
 
-        Parkplatz parkplatz = new Parkplatz(1000, 1000);
+        Parkplatz parkplatz = new Parkplatz(2000, 2000);
 
         fuehreAus();
     }
 
     public void fuehreAus() {
         while (!tastatur.esc()) {
-            if (tastatur.rechts() && bmw.gibX() < 1000) {
+            if (tastatur.rechts() && bmw.gibX() < 850) {
                 bmw.bewegeRechts();
             }
-            if (tastatur.links() && bmw.gibX() > -1000) {
+            if (tastatur.links() && bmw.gibX() > -850) {
                 bmw.bewegeLinks();
             }
-            if (tastatur.oben() && bmw.gibZ() > -1000) {
+            if (tastatur.oben() && bmw.gibZ() > -850) {
                 bmw.bewegevorne();
             }
-            if (tastatur.unten() && bmw.gibZ() < 1000) {
+            if (tastatur.unten() && bmw.gibZ() < 850) {
                 bmw.bewegehinten();
             }
             Sys.warte();
--- src/Parkplatz.java
+++ src/Parkplatz.java
@@ -1,12 +1,21 @@
 import GLOOP.*;
 public class Parkplatz {
-    private GLQuader untergrund;
+    private GLQuader untergrund,wl,wr,wh,wv;
     private double breite, tiefe;
 
     public Parkplatz(double pBreite, double pTiefe){
         breite = pBreite;
         tiefe = pTiefe;
         untergrund = new GLQuader(0,0,0, breite,20,tiefe);
-        untergrund.setzeTextur("src/img/parkplatz.jpg");
+        wh = new GLQuader(0,0,0,100,100,2000);
+        wh.verschiebe(0,25,-1000);
+        wh.drehe(0,90,0);
+        wv = new GLQuader(0,0,0,100,100,2000);
+        wv.verschiebe(0,25,1000);
+        wv.drehe(0,90,0);
+        wl = new GLQuader(0,0,0,100,100,2000);
+        wl.verschiebe(-950,25,0);
+        wr = new GLQuader(0,0,0,100,100,2000);
+        wr.verschiebe(950,25,0);
     }
 }
